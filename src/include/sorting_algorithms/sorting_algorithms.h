7d86aa79efe432ae1b6bfc304f41f6944159679a
Martin Bourbier <martin.bourbier@epita.fr>
2021-08-27 15:13:54 +0100

    sorting_algorithms header done

---
+++ src/include/sorting_algorithms/sorting_algorithms.h
@@ -0,0 +1,18 @@
+#ifndef SORTING_ALGORITHMS_H
+#define SORTING_ALGORITHMS_H
+
+#include "../data_structures/list.h"
+
+void selection_sort(list_T *list);
+
+void bubble_sort(list_T *list);
+
+void insertion_sort(list_T *list);
+
+void merge_sort(list_T *list);
+
+void quick_sort(list_T *list);
+
+void heap_sort(list_T *list);
+
+#endif
